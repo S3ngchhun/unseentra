1821a9f63f1f25abfdb8f591006bc32fcc3a4aa6
S3ngchhun <kimseng68.sc@gmail.com>
2020-03-08 14:01:15 +0700

    Added Tab Overview and enable tab switchable

--- Unseentra/js/main.js
+++ Unseentra/js/main.js
@@ -1,24 +1,61 @@
-$('header .ham_icon').click(function () {
+$('header .ham_icon').click(function() {
     $('.header_menu').toggle(150);
 
 });
 
-$('.search_modal .close_search').click(function () {
+$('.search_modal .close_search').click(function() {
     $('.search_modal').removeClass('--active');
     $('.input_search').val('');
 });
 
-$('.search').click(function () {
+$('.search').click(function() {
     $('.search_modal').addClass('--active');
     $('.input_search').focus();
 });
 
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
+$(function() {
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 10) {
             $('.header').addClass('active');
         } else {
             $('.header').removeClass('active');
         }
     });
+});
+
+$('.tab_items.overview_tab').click(function() {
+    if (!$('.tab_items.overview').hasClass('.active')) {
+        for (i = 0; i < $('.tab_items').length; i++) {
+            $('.tab_items').removeClass('active')
+        }
+        $('.tab_items.overview').addClass('active');
+        for (i = 0; i < $('.tab_content').length; i++) {
+            $('.tab_content').removeClass('active');
+        }
+        $('.tab_content.overview').addClass('active');
+    }
+});
+$('.tab_items.attractions_tab').click(function() {
+    if (!$('.tab_items.attractions_tab').hasClass('.active')) {
+        for (i = 0; i < $('.tab_items').length; i++) {
+            $('.tab_items').removeClass('active')
+        }
+        $('.tab_items.attractions_tab').addClass('active');
+        for (i = 0; i < $('.tab_content').length; i++) {
+            $('.tab_content').removeClass('active');
+        }
+        $('.tab_content.attractions_and_shows').addClass('active');
+    }
+});
+$('.tab_items.tour_tab').click(function() {
+    if (!$('.tab_items.tour_tab').hasClass('.active')) {
+        for (i = 0; i < $('.tab_items').length; i++) {
+            $('.tab_items').removeClass('active')
+        }
+        $('.tab_items.tour_tab').addClass('active');
+        for (i = 0; i < $('.tab_content').length; i++) {
+            $('.tab_content').removeClass('active');
+        }
+        $('.tab_content.tour').addClass('active');
+    }
 });
